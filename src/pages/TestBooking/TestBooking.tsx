e80e5581c39f8ebba63e1255fc53f2fee38c7d9b
Ankush <deshwalankush23@gmail.com>
2025-06-01 14:01:22 +0530

    Minor Fixes

--- src/pages/TestBooking/TestBooking.tsx
+++ src/pages/TestBooking/TestBooking.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import type { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
 import MobileNavbar from '../../components/Navbar/MobileNavbar'
 import Footer from '../../components/Footer/Footer'
