25a433237fe2f117e9237a68a3ee3afeee5ee3b6
Ankush <deshwalankush23@gmail.com>
2025-06-05 11:56:27 +0530

    Changes done

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,6 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-
-
 function App() {
     return (
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ''}>
