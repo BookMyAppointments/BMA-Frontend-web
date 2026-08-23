f9b90d5b64899510f20a168d7ab41fac768b5c78
lakshmisureshchandra <chanduvls25@gmail.com>
2026-08-23 19:45:06 +0530

    fix: whitelist seed data image hosts in next/image config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+    domains: [
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "static.vecteezy.com",
+      "img.freepik.com",
+      "media.gettyimages.com",
+      "picsum.photos",
+    ],
   },
 };
 
